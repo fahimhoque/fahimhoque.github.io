5b03ff0559c8b1d0d12adecc9d3ec46aebf85072
Fahim Hoque <fhoque.arnob@gmail.com>
2020-12-14 10:24:02 +0600

    added views

--- visareq/app/app.js
+++ visareq/app/app.js
@@ -1,4 +1,17 @@
-function testfunction()
-{
-    alert("app.js works just fine");
-}
+//Routing
+var app = angular.module("scotchy", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "/app/views/home.html"
+    })
+    .when("/about", {
+        templateUrl : "/app/views/about.html"
+    })
+    .when("/contact", {
+        templateUrl : "/app/views/contact.html"
+    })
+    .when("/blue", {
+        templateUrl : "/app/views/about.html"
+    });
+});
